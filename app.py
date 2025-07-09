561461330d1807cf4b2e7d62aa8356a14d569408
KingAbuu <godhana.ali@gmail.com>
2025-07-09 21:21:59 +0300

    Add files via upload
    Update app.py to render homepage

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, JibambeNet is running!"
+    return render_template('index.html')  # This shows your real homepage
+
+if __name__ == '__main__':
+    app.run(debug=True)
